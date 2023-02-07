b01eecc644b2d0cd6794651b265e91c6e1f52669
Juho <juho.martti@edu.turkuamk.fi>
2023-02-07 16:33:50 +0200

    part3 done

--- index.js
+++ index.js
@@ -10,41 +10,41 @@ app.use(cors())
 app.use(express.static('build'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 
-morgan.token('content', function(req, res) {
-    return JSON.stringify(req.body);
-});
+morgan.token('content', function(req) {
+  return JSON.stringify(req.body)
+})
 
 app.get('/api/persons', (request, response) => {
-    Person.find({}).then(persons => {
-      response.json(persons)
-    })
+  Person.find({}).then(persons => {
+    response.json(persons)
   })
+})
 
 app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
-  .then(person => {
-    if (person) {
-      response.json(person)
-    } else {
-      response.status(404).end()
-    }
-  })
-  .catch(error => next(error))
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
-    const date = new Date();
-    Person.find({}).then(persons => {
-      response.send(`Phonebook has info for ${persons.length} people </br> ${date}`)
-    })
+  const date = new Date()
+  Person.find({}).then(persons => {
+    response.send(`Phonebook has info for ${persons.length} people </br> ${date}`)
+  })
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
-  .then(result => {
-    response.status(204).end()
-  })
-  .catch(error => next(error))
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response, next) => {
@@ -62,7 +62,7 @@ app.post('/api/persons', (request, response, next) => {
   person.save().then(savedPerson => {
     response.json(savedPerson)
   })
-  .catch(error => next(error))
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
--- models/person.js
+++ models/person.js
@@ -6,29 +6,29 @@ mongoose.set('strictQuery', false)
 const url = process.env.MONGODB_URL
 
 mongoose.connect(url)
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minlength: 3,
-        required: true,
-    },
-    number: {
-        type: String,
-        required: true,
-        validate: {
-            validator: function(v) {
-              return /^\d{2,3}-\d{5,}$/.test(v);
-            },
-            message: props => `${props.value} is not a valid phone number!`
-        }
+  name: {
+    type: String,
+    minlength: 3,
+    required: true,
+  },
+  number: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function(v) {
+        return /^\d{2,3}-\d{5,}$/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number!`
     }
+  }
 })
 
 personSchema.set('toJSON', {
